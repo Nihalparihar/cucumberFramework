be628942824257b08d5b283289714d69fbc51096
Nihal Singh Parihar <nihalsingh.10821737@ltimindtree.com>
2025-06-10 13:54:55 +0530

    First Commit

---
+++ src/test/java/feature/login.feature
@@ -0,0 +1,37 @@
+Feature: Automate login functionality
+  Scenario: Login with valid credentials
+    Given User navigates to login page
+    When User enters valid email address "nihalsinghparihar@gmail.com" into the email field
+    And User enters valid password "parihar" into password field
+    And User clicks on login button
+    Then User should successfully get login
+
+  Scenario: Login with invalid credentials
+    Given User navigates to login page
+    When User enters invalid email address "nihalsinghparihar@gmail.com" into the email field
+    And User enters invalid password "parihar" into password field
+    And User clicks on login button
+    Then User should get proper warning message about invalid credintials
+
+  Scenario: Login with invalid email and valid password
+    Given User navigates to login page
+    When User enters invalid email address "nihalsinghparihar1@gmail.com" into the email field
+    And User enters valid password "parihar" into password field
+    And User clicks on login button
+    Then User should get proper warning message about invalid credintials
+
+  Scenario: Login with valid email and invalid password
+    Given User navigates to login page
+    When User enters valid email address "nihalsinghparihar@gmail.com" into the email field
+    And User enters invalid password "parihar1" into password field
+    And User clicks on login button
+    Then User should get proper warning message about invalid credintials
+
+  Scenario: Login without entering email and password
+    Given User navigates to login page
+    When User does not enter email into the email field
+    And User does not enter password into password field
+    And User clicks on login button
+    Then User should get proper warning message about invalid credintials
+
+
---
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -0,0 +1,65 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class LoginSteps {
+    @Given("User navigates to login page")
+    public void user_navigates_to_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters valid email address {string} into the email field")
+    public void user_enters_valid_email_address_into_the_email_field(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters valid password {string} into password field")
+    public void user_enters_valid_password_into_password_field(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User clicks on login button")
+    public void user_clicks_on_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User should successfully get login")
+    public void user_should_successfully_get_login() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters invalid email address {string} into the email field")
+    public void user_enters_invalid_email_address_into_the_email_field(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters invalid password {string} into password field")
+    public void user_enters_invalid_password_into_password_field(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User should get proper warning message about invalid credintials")
+    public void user_should_get_proper_warning_message_about_invalid_credintials() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User does not enter email into the email field")
+    public void user_does_not_enter_email_into_the_email_field() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User does not enter password into password field")
+    public void user_does_not_enter_password_into_password_field() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
